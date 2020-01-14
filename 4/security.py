38e0934f6e609e791f161dc293cde34d51944683
hamid-shahmohammadi <hamid.shahmohamadi@gmail.com>
2020-01-14 11:58:09 +0330

    5-98-10-24-1155

--- 4/security.py
+++ 4/security.py
@@ -2,19 +2,12 @@ from werkzeug.security import safe_str_cmp
 from user import User
 
 
-users = [
-    User(1, 'user1', 'abcxyz'),
-    User(2, 'user2', 'abcxyz'),
-]
-
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
 
 def authenticate(username, password):
-    user = username_table.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_table.get(user_id, None)
+    return User.find_by_id(user_id)
